1eee2bda37e6c5467e2b777e695765e67e456975
Kevin Allen <kma1660@gmail.com>
2016-02-26 23:54:46 -0500

    Changed LowBarAuto to non-PID drive backwards

--- src/Commands/Auto/LowBarForward.cpp
+++ src/Commands/Auto/LowBarForward.cpp
@@ -3,12 +3,14 @@
 #include "Commands/ArmSet.h"
 #include "Commands/Auto/DriveAuto.h"
 
-LowBarForward::LowBarForward()
+LowBarForward::LowBarForward() : CommandGroup("LowBarForward")
 {
+
 	AddSequential(new ArmSet(-0.3));
 	AddSequential(new WaitCommand(0.5));
 	AddSequential(new ArmSet(0));
-	AddSequential(new RotateX(0,-0.5));
+	//AddParallel(new RotateX(0,-0.75));
+	AddSequential(new DriveAuto(-0.75,0));
 	AddSequential(new WaitCommand(4));
 	AddSequential(new DriveAuto(0,0));
 }
