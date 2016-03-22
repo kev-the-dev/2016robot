cbc9a922d68a3b992aa45f01733c81700451a864
Kevin Allen <kma1660@gmail.com>
2016-03-22 00:13:35 -0400

    removed encoder stuff from shooter

--- src/Subsystems/Shooter.cpp
+++ src/Subsystems/Shooter.cpp
@@ -12,34 +12,11 @@ Shooter::Shooter() :
 {
 	shooterLeft = RobotMap::shooterLeft;
 	shooterRight = RobotMap::shooterRight;
-//	shooterLeftEncoder = RobotMap::shooterLeftEncoder;
-//	shooterRightEncoder = RobotMap::shooterRightEncoder;
-//
-//	//Ensure encoder uses rate, not distance
-//	shooterLeftEncoder->SetPIDSourceType(PIDSourceType::kRate);
-//	shooterRightEncoder->SetPIDSourceType(PIDSourceType::kRate);
-//
-//	leftPID.reset(new PIDController(P,I,D,shooterLeftEncoder.get(),shooterLeft.get()));
-//	rightPID.reset(new PIDController(P,I,D,shooterRightEncoder.get(),shooterRight.get()));
-//
-//	leftPID->SetOutputRange(-1,1);
-//	rightPID->SetOutputRange(-1,1);
-//
-//	leftPID->SetPercentTolerance(percentageTolerance);
-//	rightPID->SetPercentTolerance(percentageTolerance);
-//
-//	PIDenabled = false;
-//
-//	onePID.reset(new PIDController(P,I,D,shooterLeftEncoder.get(),this));
-//	onePID->SetOutputRange(-1,1);
-//	onePID->SetPercentTolerance(percentageTolerance);
-
 }
 
 void Shooter::InitDefaultCommand()
 {
-	// Set the default command for a subsystem here.
-	//SetDefaultCommand(new MySpecialCommand());
+
 }
 
 // Put methods for controlling this subsystem
@@ -49,44 +26,3 @@ void Shooter::Set(float x)
 	shooterLeft->Set(-x);
 	shooterRight->Set(x);
 }
-//void Shooter::EnablePID()
-//{
-//	leftPID->Enable();
-//	rightPID->Enable();
-//	PIDenabled = true;
-//}
-//void Shooter::DisablePID()
-//{
-//	leftPID->Disable();
-//	rightPID->Disable();
-//	PIDenabled = false;
-//}
-//bool Shooter::IsPIDEnabled()
-//{
-//	return PIDenabled;
-//}
-//double Shooter::LeftRate()
-//{
-//	return shooterLeftEncoder->GetRate();
-//}
-//double Shooter::RightRate()
-//{
-//	return shooterRightEncoder->GetRate();
-//}
-//void Shooter::PIDSet(float rate)
-//{
-//	leftPID->SetSetpoint(rate);
-//	rightPID->SetSetpoint(rate);
-//}
-//bool Shooter::LeftOnTarget()
-//{
-//	return leftPID->GetError();
-//}
-//bool Shooter::RightOnTarget()
-//{
-//	return rightPID->OnTarget();
-//}
-//void Shooter::PIDWrite(float output)
-//{
-//	Set(output);
-//}
--- src/Subsystems/Shooter.h
+++ src/Subsystems/Shooter.h
@@ -7,38 +7,12 @@
 class Shooter: public Subsystem //,PIDOutput
 {
 private:
-	// It's desirable that everything possible under private except
-	// for methods that implement subsystem capabilities
-//	static float P;
-//	static float I;
-//	static float D;
-//
-//	static float percentageTolerance;
-
 	std::shared_ptr<SpeedController> shooterLeft;
 	std::shared_ptr<SpeedController> shooterRight;
-////	std::shared_ptr<Encoder> shooterLeftEncoder;
-////	std::shared_ptr<Encoder> shooterRightEncoder;
-////	std::unique_ptr<PIDController> leftPID;
-////	std::unique_ptr<PIDController> rightPID;
-////	std::unique_ptr<PIDController> onePID;
-//
-//	bool PIDenabled;
 public:
 	Shooter();
 	void Set(float x);
 	void InitDefaultCommand();
-//	void PIDSet(float rate);
-//	void EnablePID();
-//	void DisablePID();
-//	double LeftRate();
-//	double RightRate();
-//	bool LeftOnTarget();
-//	bool RightOnTarget();
-//	bool IsPIDEnabled();
-//	void PIDWrite (float output);
-//	const double SHOOTING_RATE = 500;
-//	const double INTAKE_RATE = -200;
 };
 
 #endif
